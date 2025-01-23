3b68db1c7c67a5a16aaeeafc788435f9cf3e72c8
Your GitHub Username <khaszeeshan8@gmail.com>
2025-01-23 19:09:05 +0500

    updated

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,11 +3,15 @@ import AuthRoutes from "../modules/auth";
 import UserRoutes from "../modules/user";
 import FinanceSummaryRoutes from "../modules/finance/summary";
 import InvoiceRoutes from "../modules/invoice";
+import ChartDataRoutes from "../modules/finance/chartData";
+import BankAccountRoutes from "../modules/finance/bankAccount";
 
 const AppRoutes = (app: FastifyInstance) => {
   AuthRoutes(app)
   UserRoutes(app)
   FinanceSummaryRoutes(app)
+  ChartDataRoutes(app)
+  BankAccountRoutes(app)
   InvoiceRoutes(app)
   app.get("/", async (request, reply) => {
     reply.send({ message: "API is running" });
